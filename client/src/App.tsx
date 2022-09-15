49a24a18684a09b56ae069d970646ac4d9ab8972
justinpaulosolo <justinpaulosolo@gmail.com>
2022-09-15 15:30:50 -0700

    Basic Routing setup.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import {LoginPage} from "./pages/LoginPage";
-import {RegisterPage} from "./pages/RegisterPage";
-/*  Test */
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
