17a947a38474fb47e8e2133491d7cef3b652cec7
Justin Solo <darwin@Justins-MacBook-Pro.local>
2022-09-19 16:28:08 -0700

    Switched DB to SQLite3

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -5,12 +5,8 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
-        type: 'mysql',
-        host: process.env.MYSQL_DB_HOST,
-        port: 3306,
-        username: process.env.MYSQL_DB_USERNAME,
-        password: process.env.MYSQL_DB_PASSWORD,
-        database: process.env.MYSQL_DB_NAME,
+        type: 'sqlite',
+        database: 'meetupDB',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
